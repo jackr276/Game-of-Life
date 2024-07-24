260b4f1c3caa9549ed5785c47edfdade69564c40
jackr276 <jack_m_robbins@me.com>
2024-07-24 17:34:54 -0400

    restructure

---
+++ src/life.h
@@ -0,0 +1,12 @@
+/**
+ * Author: Jack Robbins
+ * This file contains all the needed methods for the life game to run
+ */
+
+#ifndef LIFE_H
+#define LIFE_H
+
+
+
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,11 @@
+/**
+ * Author: Jack Robbins
+ * This is the entry point for the game of life terminal display
+ */
+
+
+#include <stdio.h>
+int main(void){
+	printf("I work\n");
+	return 0;
+}
