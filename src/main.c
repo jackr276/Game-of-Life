1279c7065c396cea80c046c63d33af75c9662d43
jackr276 <jack_m_robbins@me.com>
2024-07-25 16:36:58 -0400

    reformat

--- src/main.c
+++ src/main.c
@@ -3,12 +3,15 @@
  * This is the entry point for the game of life terminal display
  */
 
-
 #include <stdio.h>
+//Ncurses library for our terminal display
 #include <ncurses.h>
+#include "life/life.h"
+
 int main(void){
 	//Initialize ncurses
 	initscr();
+
 	printf("I work\n");
 	refresh();
 
